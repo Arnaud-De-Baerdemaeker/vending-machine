f97d0b49d496316b917e784d68a53476d4be9cb7
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2023-12-13 16:46:51 +0100

    [feat] Set actions to handle the addition or removal of money

--- src/machines/vendingMachine.js
+++ src/machines/vendingMachine.js
@@ -4,12 +4,13 @@ import fetchStock from "../apiCalls/fetchStock";
 
 const vendingMachine = createMachine(
 	{
-		/** @xstate-layout N4IgpgJg5mDOIC5QQJawC4CcUCMCu6YemAxOsQHYDyAZjQNoAMAuoqAA4D2sK6KnFNiAAeiAGyMANCACeiALQAmABwA6ACwBObdvUBGRXoDMisZoCsAX0vTUGbPkLFV5TBUi0aZSlQpNWSCBcPHwCQqIIYnqqjADsynp6sUaMCerKmkbScgiasaqmyobm5vGKiox51rZoWLgERJgulB4UqigUvCgAhgA2KABe3aEUJBACYO0UAG6cANaTdnWOjc1urVNdfYPD-BQIHbMAxrsC-v5CwV1hgRHqWqqxYubPjBLK79mIenGaqinaRj3FL3RSxaogJYOBrOVzuCC+TZ8bZDEYkMCYTCcJrsXrDGjYgC2qih9ScTThGw6W36qL2BxmnBOI3OLEu3GugluiFi5j+Txe5jeqU+skQFT+YKMRk0z006kYJXU4JskNq0PJa3hiMwYG6EBk7QgvTAJDwsAxAGUwCajuhYAARbAUOYXQJXEbhRAZRQFIzqFJmDKxdQvL4IWLxR4fIWaRRGMQJJ4Q0krWEtBFtXX6w0oY2m81Wm1gO2O52uvQBDgcz3chA+v0Bt6aYOhsThqJqComH4VEO8owp9Vk1aUzOqbMGo0ms0WzAASQoc-tAFkJjI3dWQnsvfWio3Ay28m3w5l8rFGEY9MpGCpFb2h-YR+n1uPJ4a7LjujIAArYI4FnOi7LrAa7uBubLujWO51vEXb+uohhXqGmh6Oo4YGLE0TxGIMrKCk14BoOqqpjCFIZjqepTp+eK-v+gEYsBGKruu9CVuy243KAER6JolQaIm5jKAqjCYfhHbKPkKjPBeipxkCVgkcOabka+lE5qo7BYhAeB2lQBC0Ja6BMnMJDCBgwyTN0NCEJgAAUQqMIwACUYzKWRWobO+mnabp6D6f5NBGSZm5BNBXEiIgFhqCKN4Kh8aHmIo4YvEYqjKDeMZ8tKYjKo+yweWO6lTh0y6QGBYAyLOjFLsxoGsZBW6cruvHPKoEhGMJYKGDo7ZivWIYFDGiilE86i5SqNRPipnlvlRua1ZghAQBVVWFpg1q2vaTodK6jVhZxXLcTyUbxqCxiJah6H9cY5RDbhGXlOYKTJPlGqjhRWbzVMZUreu1UbcWpY7S6bFVgdzV1gYYi+mhbxCgYGX4ddOSyvkl6vLlGRaECb3Pqp2pfRpNAdCipyjKFHowcdCAXWIqgtgGJTZUq5gYUR90JlhiaSRYeMzUVRNTiTFBk2i5noJZqjWbZDlOS5bnTYVn0Tt9Iti3slPhUdkW0+UvqmIKwofKkyX9f6jAxPLI1JGh2h6IpU0FZqguqxppV4HQKBHCgYAUOgABieAUBAsAA0xS31eBWuHS1by+peiS5WhPzx+zg3STK40ZfoHz88ralCwtsCeyTPt+4Hweh+Hi0sdH7FQbHdaZY2iEXShaEYZ1vo4UY8RoZJWHEaqFCcBAcBCKR5IcZDNMGOG8iJOYDM6LoBi3ZNapKy7FF0DPtY00YpTpdlj1iLEqElOG8ZW-LiqSR8ySKvnO+F-v1O60fagWFez3PHoUQxoYWvAULQLY0IqHlAGRQL8PqFyRD0Wk5N34RTuG8Bmz0HYJnMAApIuVr4pFUAYMB15pQhiDLAl8hM3YGhQTrCIT9W5IUup3fqiE-idRFCUTICZxpiEoQTLy308wmjobuC+8FzrIT5KwnILxJSJzjGNZUMClLbzgdQ7yNFvx-m9mAMRUMAH5AAVnfCokgRPHDCGHuWVtBFDEIJARs1iqGi0mPPyAVDLGSOHMAxc9eJpWypGEwugWxs36kJbCMYromFMAGJxrtvKlWYuVdcfjdY-GUMvEwt4RoyjISoU8ih+QxgdmYbQKhN5Tw0UI4mpMkEHwho0nihQGbCSPtwjpoYMKKAVPdIUcobwqGUAklW3l1YNL2CScsDobQoGmBiHITVmnfCvPTRmHSWbmG6TdEw54PjjT5JePIR9RnwPGfUnYIxVBQHmWAAAQt0HxABhAAFt0CgMB0k8WeL6LJmRShRD4gGdmqRoy4SiM8F4D0zmaOEUuUu3tfb+yDiHeAjdZ66wSH8H+cQslJW2UJdmnVOaJjiOUI+xEnbvSoRsQkTzXkdDAAAUUJOwdASymkfx4niohCpcpggSIkPu19FSPDAWkSBwJVHWCAA */
+		/** @xstate-layout N4IgpgJg5mDOIC5QDcwDsIEs1QLIEMBjAC2zAGIAXAVwCc0B5AMyYG0AGAXUVAAcB7WJkqZ+aHiAAeiACwAmADQgAnogAcARgB0MgJz797AOzs5RuTIC+lpagzY8RUmjBaa9SMyZU6jNB24kEAEhETEJaQQNNSMtOXYAZmizXSNUowSlVSjTWLkYgFZdBIKjIw0ANnY1a1t0LBwCEjI3X080H3ovAIkQ4VFxIMjovMTktImMrMRUiq0jGKqEhPZTAorakDsGx2aXVo8IBjQtbH78ABtMAC98MI6IMVdsZH4Aa1dthybnV3cXI4nM4iS43O4DBAvfiEcFiAI9IJ9e4RRAFDRyLTogrsfQVOQaBJqAoyaYIArkrS6AorPEJGTVNJyTZfRpOFr-dqnNDnK63e7kMC0Wj8WhaXgXO5MEUAWy0LN2vwOAOOXJ5YPukLQrxh93hXF6gn64SGqLkGKxON0eIJRJJKlkeMp+jUajkePYehkCWZ9W+bP2HMBWkIYko6EoADEwJQSJByAi+IbkSaECUElp2Hjqa7qUUKmpSQSChj6RUNPSNKVdDJzD77Ky9n82kGQ2gw22ozHiHHWBpAonQgMUamKunM3JsxOSlaC-ach7MesLGYyvjdHWdj92c2VYLhbRO3soABhUPh8i0aO0ZQJ4JJocp4vmieW62E4mkmTorTY1bor1FOWRgFBufqNkqnKXvgEDKKcEAXBQ1CwIKACSaDIbQlCwLgTw3vqiL3saoCRBUNaLqR5L-vSdKFmicyVmWJjlgkZhqBsNhbL6DaKoGKpQTBWj4NK-DUG2aEYWGEDkI8+xQh8cpcQq26HHxYDQbBQkiWJ6GCpJmrarC-hcLeSIPsRiBAdoS7mOk7AEkYFSFhRWjLPSxgLHIVrAaB3HKcqJz8RpwmiZQ4m6XGe4imKEqUFKtCyvKW4BjuAVqQJmkhWFmGQPp0KGXq-Z3oORFSBZNYYnIdKlPIwEaFStGpFoajLNWCzoisMg1BxiX+k2KmpepgnBdpElxkhgoAMpgAhhBYQAIrQ2BvCZhGDOZCC0loWYenZxa6OwBSFsk5GVQ5boJPoPlKcl-VaIFQ1aaFOnZVJ420FNM3zYtaDLX2BrFWtpUIDZP7FPtah-sYZa0RUcwVEuLE1uSCxGFdSV9f5d1pUFj1ZZJ5BvXj2G4StAPDuWxYZn+JjLKRX60WRrnuXiq7UmjvUQUG93IZ9AzkMgoIQHcYAfWAs0DKTRqAyRlVbS+FZ7QdhaVRi8NlsYmg4gsIHdYp6Oc6pg082L-IC1cQthqL4twn9BFkymm3bQrnlK3OGgEtoasaMBnn0roLrs+BvEDQJvDChA1CzQw1CUMwE2UNCbzkJIsCUMLglMGGtAABS-uwACU-N6xzwdY4NYf8BHUcx3HCeEMt+EDlLw4g0UF3VJDDkaE53sndWEMowHuv1tdGOQdjWgRtgoJ8nzkvJutxLpgxCRlHViQVLopLxJoGbmESlYyF+eiBzxKVlwJU9oDPhnJ6n6f4Jngq56sBdFyP+ul-dV837qjdFc3R8phMQvlxPid8dpsjmHYFtJc6JjBt08qfPy49BrYFgNQFgmBCCYHDBGUSEBYDxn-qZEqkRyjpjRKUckyxzqHTdrDWIpR8j4jMDtNQVhNhoErnACQPVGz-UAetAAtI5OcojkE3WVCwQRC8galFJP7DEMQcRejsroM0HDJFj0BLIsyQN8gFAzFmQkU48yzmyCxIxr87K03WPIBI7E6gfxLufYEmBf76NIdLRAywxwmJzNOfMn5yxbT0FSFimhSJ0m0QbE4rZ2yRmjLGCAeiyGIFhjIFyXp9DRDsi6dYpJSixAqBMb2GgGSaHXMPTcrjbqRQPMk5wJ4zxtjST4jaGQqZ2QqXTD0VpPz4gzM1N0qwOEVJkDrZxtSg7n0Cu04cR9YgrzXvtRxahUiFg9Fkhi3s8SVVdKRWJX8J6YHgmABZj58jkVXuUNZm9aKVFgdaTQEMd6oxqWBM+t17oZRGuFVJdshFAwqSUJqrorRJA0VQ+h2RvZejiDEEY2JljFg+dMr5KCuYT2NtbQG3jFn+xuasjeGyjBHVdM88slZizZnRZxFxsyfkTwrlXWONcmDx0Tpcxe1yVl3I3lvBh1Q96GMJEkcsVpjlzInj-XkhkeVAz0EY-l691mbLdi6NQoqXT-gqeUJknzfJSNQZfae8r7haAgN9N4c1pqYFQNeRVkQl7EoFY4oVcLUhGOYS6MYBIvROIZTM75mNv7mvVAMLQUAHVgAAEJEDeMeYg+AcAXKBXIyIMRdAZiSPSaqBT6pzh3jA4w+RTAsVIqsKZwbMUmuxWg9CmCmDYNwR2Ah8AM36JdcsN1aqHkMNhj+GylRKhVWlbdaUvUACi0peCUGyE3TNpojH7WicYDRxgj5bLsidaBMh4ZUnYtYIAA */
 		context: {
 			welcomeMessage: null,
 			drinks: null,
 			totalDrinks: null,
 			errorFetching: null,
+			insertedAmount: 0,
 		},
 		id: "vendingMachine",
 		initial: "turnedOff",
@@ -31,8 +32,8 @@ const vendingMachine = createMachine(
 								{
 									target: "contentFetched",
 									actions: assign({
-										drinks: ({context, event}) => event.output,
-										totalDrinks: ({context, event}) => {
+										drinks: ({_, event}) => event.output,
+										totalDrinks: ({_, event}) => {
 											let
 												total = null,
 												entries = event.output,
@@ -52,7 +53,7 @@ const vendingMachine = createMachine(
 								{
 									target: "errorFetchingContent",
 									actions: assign({
-										drinks: ({context, event}) => event.error,
+										drinks: ({_, event}) => event.error,
 									}),
 								},
 							],
@@ -85,6 +86,11 @@ const vendingMachine = createMachine(
 								on: {
 									userInsertsMoney: {
 										target: "amountInserted",
+										actions: assign({
+											insertedAmount: ({context, event}) => (
+												context.insertedAmount = context.insertedAmount + event.value
+											),
+										}),
 									},
 								},
 							},
@@ -101,7 +107,20 @@ const vendingMachine = createMachine(
 									],
 									userInsertsMoney: {
 										target: "amountInserted",
+										actions: assign({
+											insertedAmount: ({context, event}) => (
+												context.insertedAmount = context.insertedAmount + event.value
+											),
+										})
 									},
+									returnMoney: {
+										target: "idle",
+										actions: assign({
+											insertedAmount: ({context, event}) => (
+												context.insertedAmount = 0
+											),
+										})
+									}
 								},
 							},
 							selection: {
