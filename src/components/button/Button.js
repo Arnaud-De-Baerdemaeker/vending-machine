22bf2a82e43e303c9d82185d3db976593f40c202
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2024-01-03 15:54:25 +0100

    [feat] Add the class props

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,8 +1,9 @@
-const Button = ({clickFunction, children, isDisabled}) => {
+const Button = ({clickFunction, children, isDisabled, buttonClass}) => {
 	return(
 		<button
 			onClick={clickFunction}
 			disabled={isDisabled}
+			className={buttonClass}
 		>
 			{children}
 		</button>
