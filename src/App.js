ed95c1be4f27f92b41d8f465804d0e46fb4adc67
Arnaud De Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2024-01-04 13:08:45 +0100

    [refactor] Rework the structure
    - Add the classes
    - Gather the different functions in their own components

--- src/App.js
+++ src/App.js
@@ -1,80 +1,72 @@
-import {useEffect} from 'react';
-import {useMachine} from '@xstate/react';
+import {useMachine} from "@xstate/react";
 
-import vendingMachine from './machines/vendingMachine';
+import vendingMachine from "./machines/vendingMachine";
 
-import Button from './components/Button';
-import Drink from './components/Drink';
-
-import './App.css';
+import Switches from "./components/switches/Switches";
+import Button from "./components/button/Button";
+import ChangeMachine from "./components/changeMachine/ChangeMachine";
+import Drink from "./components/drink/Drink";
+import { useEffect } from "react";
 
 const App = () => {
-	const [state, send] = useMachine(vendingMachine);
+	const [state, send, service] = useMachine(vendingMachine);
 
-	const coins = [0.05, 0.10, 0.20, 0.50, 1, 2];
-
-	useEffect(() => {console.log(state)}, [state]);
+	useEffect(() => {
+		console.log(state);
+	}, [state]);
 
 	return (
-		<main className="App">
-			<div>
-				<Button
-					clickFunction={() => {
-						state.matches("turnedOff") ? send({type: "turnOn"}) : alert("Machine is already on !")
-					}}
-				>
-					ON
-				</Button>
-				<Button
-					clickFunction={() => {
-						!state.matches("turnedOff") ? send({type: "turnOff"}) : alert("Machine is already off !")
-					}}
-				>
-					OFF
-				</Button>
-			</div>
+		<main className="w-[1000px] flex flex-col gap-y-[50px]">
+			<Switches
+				state={state}
+				service={service}
+			/>
 
 			{state.matches({turnedOn: "ready"})
 				&& (
-					<div>
-						<div>
-							{state.context.drinks.map(drink => (
-								<Drink
-									key={drink.id}
-									drinkName={drink.name}
-									drinkPrice={drink.price}
-									drinkStock={drink.stock}
-								/>
-							))}
-						</div>
-
-						<div>
-							<div>{state.context.insertedAmount}€</div>
-
-							<div>
-								{coins.map((coin, index) => (
-									<Button
-										key={index}
-										id={coin}
-										clickFunction={() => {
-											send({
-												type: "userInsertsMoney",
-												value: coin
-											});
-										}}
-									>
-										{coin}€
-									</Button>
+					<div className="flex flex-row gap-x-[50px]">
+						<div className="flex flex-col gap-y-[25px]">
+							<div className="flex flex-wrap gap-10 justify-between">
+								{state.context.drinks.map(drink => (
+									<Drink
+										key={drink.id}
+										state={state}
+										drinkId={drink.id}
+										drinkName={drink.name}
+										drinkPrice={drink.price}
+										drinkStock={drink.stock}
+										service={service}
+									/>
 								))}
+							</div>
+
+							<div className="flex justify-center gap-10">
 								<Button
 									clickFunction={() => {
-										send({type: "returnMoney"});
+										send({type: "VALIDATE_SELECTION"});
 									}}
+									isDisabled={state.can({type: "VALIDATE_SELECTION"}) ? false : true}
+									buttonClass={`w-40 h-10 rounded transition-all duration-500 ${state.matches({turnedOn: {ready: "selection"}}) ? "shadow-lg hover:shadow-2xl bg-slate-700 text-slate-100" : "bg-neutral-300 text-neutral-500 shadow-none"}`}
 								>
-									Annuler
+									Valider sélection
+								</Button>
+
+								<Button
+									clickFunction={() => {
+										send({type: "CANCEL_SELECTION"});
+									}}
+									isDisabled={state.can({type: "CANCEL_SELECTION"}) ? false : true}
+									buttonClass={`w-40 h-10 border-solid border rounded transition-all duration-500 ${state.matches({turnedOn: {ready: "selection"}}) ? "shadow-lg hover:shadow-2xl border-slate-700 text-slate-700" : "border-neutral-300 text-neutral-300 shadow-none"}`}
+								>
+									Annuler sélection
 								</Button>
 							</div>
 						</div>
+
+						<ChangeMachine
+							state={state}
+							service={service}
+						/>
 					</div>
 				)
 			}
